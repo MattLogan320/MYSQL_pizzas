a507af24c44d3242b06f988b50b6880fa196f4d7
MattLogan320 <matt.logan320@gmail.com>
2021-08-19 10:41:59 +0100

    added cinemas sql file

---
+++ cinema.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE cinemas;
+USE cinemas;
+
+-- We need to create our tables
+
+CREATE TABLE movies(
+	movie_id INT AUTO_INCREMENT,
+	movie_name VARCHAR(100) NOT NULL,
+    price DECIMAL NOT NULL,
+    age_restricted BOOLEAN NOT NULL,
+    runtime TIME NOT NULL,
+    screening_time DATETIME NOT NULL,
+	PRIMARY KEY (movie_id)
+);
+
+
+CREATE TABLE customers(
+    customer_id INT AUTO_INCREMENT,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    PRIMARY KEY (customer_id)
+);
+
+
+CREATE TABLE orders (
+    order_id INT AUTO_INCREMENT,
+    fk_movie_id INT,
+    fk_customer_id INT,
+    date_booking DATETIME NOT NULL,
+    screen BIT NOT NULL,
+    seat_number TINYINT NOT NULL,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (fk_movie_id) REFERENCES movies(movie_id),
+    FOREIGN KEY (fk_customer_id) REFERENCES customers(customer_id)
+);
