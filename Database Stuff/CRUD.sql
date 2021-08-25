a6d347293d28318280a2b017a9bbd729d5892918
MattLogan320 <matt.logan320@gmail.com>
2021-08-25 10:39:22 +0100

    object oriented files added

---
+++ Database Stuff/CRUD.sql
@@ -0,0 +1,8 @@
+INSERT INTO movies (movie_name, price, age_restricted, runtime, screening_time)
+VALUES ('The Suicide Squad', 9.50, TRUE, '02:13:00.00000','2021-08-19 16:35:00.00000'),
+('Space Jam: A New Legacy', 8.75, FALSE, '02:04:27.00000', '2021-08-19 20:00:00.00000'),
+('Free Guy', 9.50, TRUE, '01:55:38.00000', '2021-08-20 10:30:00.00000'),
+('Jungle Cruise',8.50, FALSE, '02:42:46.00000', '2021-08-20 12:00:00.00000'),
+('Snake Eyes', 12.50, TRUE, '02:01:30.00000', '2021-08-20 17:30:00.00000')
+;
+
